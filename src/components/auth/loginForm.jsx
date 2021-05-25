7f7a1369b1f0ddf7b904826a15ecb3ba47051075
Soroush81@gmail.com <soroush81@gmail.com>
2021-05-25 15:52:24 +0430

    changes'

--- src/components/auth/loginForm.jsx
+++ src/components/auth/loginForm.jsx
@@ -2,28 +2,86 @@ import React, { useState } from 'react'
 import { Paper, Grid, Button } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
 import FormInput from '../common/formInput'
+import Joi from 'joi-browser'
 
 const LoginForm = () => {
 
-    const [userName, setUserName] = useState("")
-    const [password, setPassword] = useState("")
+    const [account, setAccount] = useState({ username: '', password: '' })
+    const [errors, setErrors] = useState({})
+
+    const schema = {
+        username: Joi.string().required().label('Username'),
+        password: Joi.string().required().label('Password')
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(userName);
+        setErrors(validate());
+        if (errors) return;
     }
 
+    const validate = () => {
+        const options = { abortEarly: false };
+        const { error } = Joi.validate(account, schema, options)
+        if (!error) return;
+        const errs = {}
+        for (let err of error.details) {
+            errs[err.path[0]] = err.message;
+        }
+        // return (Object.keys(errs).length === 0) ? null : errs;
+        return errs;
+    }
+
+    const validateProperty = ({ name, value }) => {
+        const obj = { [name]: value };
+        const newSchema = { [name]: schema[name] };
+        const { error } = Joi.validate(obj, newSchema);
+        if (!error)
+            return null;
+        return error.details[0].message;
+    }
+
+
+    const changeHandler = ({ target: input }) => {
+        const errorMessage = validateProperty(input);
+        const newErrorObj = { ...errors };
+        if (errorMessage) newErrorObj[input.name] = errorMessage;
+        else delete newErrorObj[input.name];
+
+        setErrors(newErrorObj);
+        const newAccountObj = { ...account };
+        newAccountObj[input.name] = input.value;
+        setAccount(newAccountObj);
+    }
     const methods = useForm();
     return (
         <>
             <FormProvider {...methods}>
-                <form style={{ margin: "0 auto", width: "450px" }} onSubmit={handleSubmit}>
+                <form style={{ margin: "0 auto", width: "450px" }} onSubmit={handleSubmit} noValidate>
                     <Paper style={{ padding: 16 }} variant="outlined">
                         <Grid container alignItems="flex-start" spacing={2}>
-                            <FormInput name="userName" autoFocus="true" label="UserName" value={userName} onChange={(event) => { setUserName(event.target.value) }} required size="12" />
-                            <FormInput name="password" label="Password" value={password} onChange={(event) => { setPassword(event.target.value) }} required size="12" />
                             <Grid item style={{ marginTop: 16 }}>
-                                <Button variant="contained" color="primary" type="submit">Submit</Button>
+                            </Grid>
+                            <FormInput
+                                name="username"
+                                label="UserName"
+                                value={account.username}
+                                onChange={changeHandler}
+                                required
+                                size={12}
+                                autoFocus={true}
+                                error={errors && errors.username} />
+                            <FormInput
+                                name="password"
+                                label="Password"
+                                value={account.password}
+                                onChange={changeHandler}
+                                required
+                                size={12}
+                                error={errors && errors.password} />
+
+                            <Grid item style={{ marginTop: 16 }}>
+                                <Button variant="contained" color="primary" type="submit" disabled={validate()}>Submit</Button>
                             </Grid>
                         </Grid>
                     </Paper>
--- src/components/common/formInput.jsx
+++ src/components/common/formInput.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useFormContext, Controller } from 'react-hook-form';
-import { TextField, Grid } from '@material-ui/core';
+import { TextField, Grid, Typography } from '@material-ui/core';
 
-function FormInput({ name, label, required, size, value, onChange, autoFocus }) {
+function FormInput({ name, label, required, size, value, onChange, autoFocus, error }) {
     const { control } = useFormContext();
     return (
         <Grid item xs={size}>
@@ -15,6 +15,9 @@ function FormInput({ name, label, required, size, value, onChange, autoFocus }) 
                         value={value}
                         onChange={onChange}
                         autoFocus={autoFocus}
+                        name={name}
+                        error={error != undefined}
+                        helperText={error && (value === "" ? error : ' ')}
                     />
                 )}
                 name={name}
